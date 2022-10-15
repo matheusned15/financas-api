203b292577e5a7f076214c0ee4233f4373bf2cf1
Matheus Ned <matheusned15@gmail.com>
2022-10-15 20:33:28 -0300

    Implementação do POST de usuários

--- src/main/java/com/nedstore/minhasfinancasapi/api/resources/UsuarioResource.java
+++ src/main/java/com/nedstore/minhasfinancasapi/api/resources/UsuarioResource.java
@@ -1,13 +1,36 @@
 package com.nedstore.minhasfinancasapi.api.resources;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.nedstore.minhasfinancasapi.api.dto.UsuarioDTO;
+import com.nedstore.minhasfinancasapi.exception.RegraNegocioException;
+import com.nedstore.minhasfinancasapi.model.entity.Usuario;
+import com.nedstore.minhasfinancasapi.service.UsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/usuarios")
 public class UsuarioResource {
 
-    @GetMapping("/")
-    public String hello(){
-        return "hello world!";
+    private UsuarioService service;
+
+    @PostMapping
+    public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
+
+        Usuario usuario = Usuario.builder()
+                .nome(dto.getNome())
+                .email(dto.getEmail())
+                .senha(dto.getSenha()).build();
+
+        try {
+            Usuario usuarioSalvo = service.salvarUsuario(usuario);
+            return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
     }
 }
