952db67fdd252d9f1f814fb620550be325e8245e
Matheus Ned <matheusned15@gmail.com>
2022-10-15 20:51:32 -0300

    Implementando o Endpoint de autenticação de usuários

--- src/main/java/com/nedstore/minhasfinancasapi/api/resources/UsuarioResource.java
+++ src/main/java/com/nedstore/minhasfinancasapi/api/resources/UsuarioResource.java
@@ -1,9 +1,11 @@
 package com.nedstore.minhasfinancasapi.api.resources;
 
 import com.nedstore.minhasfinancasapi.api.dto.UsuarioDTO;
+import com.nedstore.minhasfinancasapi.exception.ErroAutenticacao;
 import com.nedstore.minhasfinancasapi.exception.RegraNegocioException;
 import com.nedstore.minhasfinancasapi.model.entity.Usuario;
 import com.nedstore.minhasfinancasapi.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/usuarios")
 public class UsuarioResource {
 
+    @Autowired
     private UsuarioService service;
+
+    public ResponseEntity autenticar(@RequestBody UsuarioDTO dto) {
+        try {
+            Usuario usuarioAutenticado = service.autenticar(dto.getEmail(), dto.getSenha());
+            return ResponseEntity.ok(usuarioAutenticado);
+        } catch (ErroAutenticacao e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 
     @PostMapping
     public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
