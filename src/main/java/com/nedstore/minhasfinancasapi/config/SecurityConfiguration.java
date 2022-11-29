9645414da9116e9f4291012dc47d1410857ec182
Matheus Ned <matheusned15@gmail.com>
2022-11-29 15:46:45 -0300

    Configurando o CorsFilter para o Security

--- src/main/java/com/nedstore/minhasfinancasapi/config/SecurityConfiguration.java
+++ src/main/java/com/nedstore/minhasfinancasapi/config/SecurityConfiguration.java
@@ -1,8 +1,12 @@
 package com.nedstore.minhasfinancasapi.config;
 
+import com.nedstore.minhasfinancasapi.api.JwtTokenFilter;
+import com.nedstore.minhasfinancasapi.service.JwtService;
 import com.nedstore.minhasfinancasapi.service.impl.SecurityUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,17 +15,34 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
+
+
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private SecurityUserDetailsService userDetailsService;
+    @Autowired
+    private JwtService jwtService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         PasswordEncoder encoder = new BCryptPasswordEncoder();
         return encoder;
+    }
+
+    @Bean
+    public JwtTokenFilter jwtTokenFilter() {
+        return new JwtTokenFilter(jwtService, userDetailsService);
     }
 
     @Override
@@ -36,12 +57,39 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/usuarios/autenticar").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/usuarios").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .httpBasic();
+                .addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class)
+        ;
+
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+
+        List<String> all = Arrays.asList("*");
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedMethods(all);
+        config.setAllowedOrigins(all);
+        config.setAllowedHeaders(all);
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        CorsFilter corFilter = new CorsFilter(source);
+
+        FilterRegistrationBean<CorsFilter> filter =
+                new FilterRegistrationBean<CorsFilter>(corFilter);
+        filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return filter;
     }
 }
